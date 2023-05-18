3288a7e2d66c91d5aa53151583703ce1c919da50
R.M.Y.P.Rathnayake <122531660+asp2020100@users.noreply.github.com>
2023-05-18 11:09:59 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,13 +1,36 @@
-const text = "Hello, I'm Yasas Pamudu !";
+
+const phrases = ["Hello, I'm Yasas Pamudu !"];
 let index = 0;
+let phraseIndex = 0;
+const typingDelay = 100; // Delay between each character typing
+const eraseDelay = 3000; // Delay before erasing the text
+const phraseDelay = 2000; // Delay before typing the next phrase
+
+const textElement = document.querySelector('.typing .text');
 
 function type() {
-  const span = document.querySelector('.typing .text');
-  if (index < text.length) {
-    span.textContent += text.charAt(index);
+  if (index < phrases[phraseIndex].length) {
+    textElement.textContent += phrases[phraseIndex].charAt(index);
     index++;
-    setTimeout(type, 100);
+    setTimeout(type, typingDelay);
+  } else {
+    setTimeout(erase, eraseDelay);
   }
 }
 
+function erase() {
+  if (index > 0) {
+    textElement.textContent = phrases[phraseIndex].substring(0, index - 1);
+    index--;
+    setTimeout(erase, typingDelay);
+  } else {
+    phraseIndex++;
+    if (phraseIndex >= phrases.length) {
+      phraseIndex = 0;
+    }
+    setTimeout(type, phraseDelay);
+  }
+}
+
+// Start the typing effect
 type();
